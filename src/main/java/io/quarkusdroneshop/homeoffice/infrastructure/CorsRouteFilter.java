4cc1f282d5c773ba532f21e611ccc1e4464460c2
Noriaki Mushino <nmushino@redhat.com>
2026-06-29 05:23:24 +0900

    Update of CORS Header

---
+++ src/main/java/io/quarkusdroneshop/homeoffice/infrastructure/CorsRouteFilter.java
@@ -0,0 +1,26 @@
+package io.quarkusdroneshop.homeoffice.infrastructure;
+
+import io.quarkus.vertx.web.RouteFilter;
+import io.vertx.ext.web.RoutingContext;
+
+public class CorsRouteFilter {
+
+    @RouteFilter(Integer.MAX_VALUE)
+    void corsFilter(RoutingContext rc) {
+        String origin = rc.request().getHeader("Origin");
+        if (origin != null) {
+            rc.response()
+                .putHeader("Access-Control-Allow-Origin", "*")
+                .putHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+                .putHeader("Access-Control-Allow-Headers", "Content-Type,Authorization")
+                .putHeader("Access-Control-Allow-Credentials", "false");
+        }
+
+        if ("OPTIONS".equalsIgnoreCase(rc.request().method().name())) {
+            rc.response().setStatusCode(200).end();
+            return;
+        }
+
+        rc.next();
+    }
+}
